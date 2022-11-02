28070ab1357e5162a535d40b128ae1f5063ceda7
Gonçalo <Gonalo@DESKTOP-R7KFHB9>
2022-11-02 01:53:46 +0100

    Progreso: implementación metodo checkForm que verifica json y si todo lo insertado es correcto, y parte de view, implementacion textarea como en iterativas 08 para que se pueda insertar el texto a verificar.

--- controllers/ejercicioCalculoNotas.controller.php
+++ controllers/ejercicioCalculoNotas.controller.php
@@ -1,6 +1,56 @@
 <?php
+
 declare(strict_types=1);
 
+if (isset($_POST['enviar'])) {
+    $data['errores'] = checkForm($_POST);
+    $data['input'] = filter_var_array($_POST);
+    var_dump($data);
+    if (count($data['errores']) === 0) {
+        $jsonArray = json_decode($_POST['json_notas'], true);
+        //$resultado = $jsonArray;
+        //$data['resultado'] = $resultado;
+    }
+}
+
+function checkForm(array $post): array {
+    $errores = [];
+    if (empty($post['json_notas'])) {
+        $errores['json_notas'] = 'Este campo es obligatorio';
+    } else {
+        $modulos = json_decode($post['json_notas'], true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $errores['json_notas'] = 'El formato no es correcto';
+        } else {
+            $erroresJson = "";
+            foreach ($modulos as $modulo => $alumnos) {
+                if (empty($modulo)) {
+                    $erroresJson .= "El nombre del módulo no puede estar vacío<br>";
+                } else {
+                    foreach ($alumnos as $nombre => $notas) {
+                        foreach ($notas as $nota) {
+                            if (empty($nombre)) {
+                                $erroresJson .= "El módulo '" . htmlentities($modulo) . "' tiene un alumno sin nombre<br>"; //Equivale a filter_var($modulo, FILTER_SANITIZE_SPECIAL_CHARS);
+                            } else {
+                                if (!is_numeric($nota)) {
+                                    $erroresJson .= "El módulo '" . htmlentities($modulo) . "' el/la alumno/a '" . htmlentities($nombre) . "' tiene la nota '" . htmlentities($nota) . "' que no es un numero<br>"; //Equivale a filter_var($modulo, FILTER_SANITIZE_SPECIAL_CHARS);
+                                } else {
+                                    if ($nota < 0 || $nota > 10) {
+                                        $erroresJson .= "Módulo '" . htmlentities($modulo) . "' alumno '" . htmlentities($nombre) . "' tiene una nota de " . $nota . "<br>"; //Equivale a filter_var($modulo, FILTER_SANITIZE_SPECIAL_CHARS);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!empty($erroresJson)) {
+                $errores['json_notas'] = $erroresJson;
+            }
+        }
+    }
+    return $errores;
+}
 
 include 'views/templates/header.php';
 include 'views/ejercicioCalculoNotas.view.php';
--- views/ejercicioCalculoNotas.view.php
+++ views/ejercicioCalculoNotas.view.php
@@ -1,31 +1,30 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Calculo Notas</h1>
 
-</div>
-<!-- Content Row -->
 
-<div class="row">
-
-</div>
-
-<!-- comment -->
-<div class="col-12">
-    <div class="card shadow mb-4">
-        <div
-            class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="m-0 font-weight-bold text-primary">Calculo Notas</h6>                                    
+    
+    <!-- comment -->
+    <div class="col-12">
+        <div class="card shadow mb-4">
+            <div
+                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Array de notas</h6>                                    
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <!--<form action="./?sec=formulario" method="post">                   -->
+                <form method="post" action="./?sec=ejercicioCalculoNotas">
+                    <!--<input type="hidden" name="sec" value="iterativas01" />-->
+                    <div class="mb-3">
+                        <label for="texto">Json Notas:</label>
+                        <textarea class="form-control" id="json_notas" name="json_notas" rows="10"><?php echo isset($data['input']['json_notas']) ? $data['input']['json_notas'] : ''; ?></textarea>
+                        <p class="text-danger small"><?php echo isset($data['errores']['json_notas']) ? $data['errores']['json_notas'] : ''; ?></p>
+                    </div>                    
+                    <div class="mb-3">
+                        <input type="submit" value="Enviar" name="enviar" class="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
         </div>
-        <!-- Card Body -->
-        <div class="card-body">
-            <!--<form action="./?sec=formulario" method="post">                   -->
-            <form method="post" action="./?sec=ejercicioCalculoNotas">
-                <!--<input type="hidden" name="sec" value="iterativas01" />-->
-                <div class="mb-3">
-                    <input type="submit" value="Enviar" name="enviar" class="btn btn-primary"/>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>                        
+    </div>                        
 </div>
